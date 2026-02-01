f50d04d5e7821518a8f93380f60c90038e0ab0f8
Trina <92634994+trinayau@users.noreply.github.com>
2026-02-01 14:37:17 +0000

    add meta description

--- sw.js
+++ sw.js
@@ -1,42 +1,48 @@
-const CACHE_NAME = 'meemtime-v20260201';
+const CACHE_NAME = "meemtime-v20260201";
 const ASSETS = [
-  './',
-  './index.html',
-  './css/styles.css',
-  './css/relaxation.css',
-  './js/app.js',
-  './js/timer-logic.js',
-  './js/timer-display.js',
-  './js/relaxation-data.js',
-  './js/relaxation-logic.js',
-  './js/relaxation-app.js',
-  './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  "./",
+  "./index.html",
+  "./css/styles.css",
+  "./css/relaxation.css",
+  "./js/app.js",
+  "./js/timer-logic.js",
+  "./js/timer-display.js",
+  "./js/relaxation-data.js",
+  "./js/relaxation-logic.js",
+  "./js/relaxation-app.js",
+  "./manifest.json",
+  "./icons/favicon.svg",
+  "./icons/favicon-96x96.png",
+  "./icons/favicon.ico",
+  "./icons/apple-touch-icon.png",
+  "./icons/web-app-manifest-192x192.png",
+  "./icons/web-app-manifest-512x512.png",
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)),
   );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) => {
       return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key)),
       );
-    })
+    }),
   );
   self.clients.claim();
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
-    })
+    }),
   );
 });
